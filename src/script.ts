7be56b0d766ed2890d9b53bb05175fe92f73b5d7
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-05-27 11:27:25 +0300

    feat: add random color paint mode

--- src/script.ts
+++ src/script.ts
@@ -5,17 +5,21 @@ const paintColorInput = document.querySelector<HTMLInputElement>("#paint-color")
 const gridSizeLabel = document.querySelector<HTMLLabelElement>("label[for='grid-size']")!;
 const gridSizeInput = document.querySelector<HTMLInputElement>("#grid-size")!;
 
+const customColorBtn = document.querySelector<HTMLButtonElement>(".controls__btn--custom-color")!;
+const randomColorBtn = document.querySelector<HTMLButtonElement>(".controls__btn--random-color")!;
+
 const gridSizes = [8, 16, 32, 48, 64] as const;
 
-type PaintingMode = "custom-color" | "random-color" | "eraser";
+type PaintMode = "custom-color" | "random-color" | "eraser";
 
 const state = {
   gridSize: gridSizes[gridSizeInput.valueAsNumber],
   canvasSize: 576,
   canvasColor: "#fff",
   gridlinesColor: "#aaa",
-  paintingMode: "custom-color" as PaintingMode,
+  paintMode: "custom-color" as PaintMode,
   paintColor: paintColorInput.value,
+  painted: null as string | null
 };
 
 function drawCanvas() {
@@ -54,6 +58,10 @@ function updateGridSize(e: Event) {
   }
 }
 
+function setPaintMode(mode: PaintMode) {
+  state.paintMode = mode;
+}
+
 function paint(e: PointerEvent) {
   if (e.buttons !== 1) return;
 
@@ -70,26 +78,51 @@ function paint(e: PointerEvent) {
   const col = Math.floor(x / cellSize);
   const row = Math.floor(y / cellSize);
 
+  const cell = `${col}-${row}`;
+  if (cell === state.painted) return;
+
+  state.painted = cell;
+
   const drawX = col * cellSize + 1;
   const drawY = row * cellSize + 1;
 
   const fillSize = cellSize - 1;
 
-  switch (state.paintingMode) {
+  switch (state.paintMode) {
     case "custom-color":
       ctx.fillStyle = state.paintColor;
+      break;
+    case "random-color":
+      ctx.fillStyle = getRandomColor();
       break;
   }
 
   ctx.fillRect(drawX, drawY, fillSize, fillSize);
 }
 
+function resetPainted() {
+  state.painted = null;
+}
+
+function getRandomColor(): string {
+  const randomNumber = Math.floor(Math.random() * 0xffffff);
+  const hexCode = `#${randomNumber.toString(16).padStart(6, "0")}`;
+
+  return hexCode;
+}
+
 function setupEvents() {
   canvas.addEventListener("pointerdown", paint);
   canvas.addEventListener("pointermove", paint);
+  canvas.addEventListener("pointerup", resetPainted);
+  canvas.addEventListener("pointerleave", resetPainted);
+
   paintColorInput.addEventListener("input", updatePaintColor);
   gridSizeInput.addEventListener("input", updateGridSize);
   gridSizeInput.addEventListener("change", updateGridSize);
+
+  customColorBtn.addEventListener("click", () => setPaintMode("custom-color"));
+  randomColorBtn.addEventListener("click", () => setPaintMode("random-color"));
 }
 
 function init() {
