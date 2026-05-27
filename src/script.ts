a5c05f8961f9d4af559d7e01cdf923c7d6c8f77f
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-05-27 08:22:34 +0300

    feat: add custom color painting mode

--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,21 @@
 const canvas = document.querySelector<HTMLCanvasElement>(".canvas")!;
 const ctx = canvas.getContext("2d")!;
 
+const paintColorInput = document.querySelector<HTMLInputElement>("#paint-color")!;
 const gridSizeLabel = document.querySelector<HTMLLabelElement>("label[for='grid-size']")!;
 const gridSizeInput = document.querySelector<HTMLInputElement>("#grid-size")!;
 
 const gridSizes = [8, 16, 32, 48, 64] as const;
+
+type PaintingMode = "custom-color" | "random-color" | "eraser";
 
 const state = {
   gridSize: gridSizes[gridSizeInput.valueAsNumber],
   canvasSize: 576,
   canvasColor: "#fff",
   gridlinesColor: "#aaa",
+  paintingMode: "custom-color" as PaintingMode,
+  paintColor: paintColorInput.value,
 };
 
 function drawCanvas() {
@@ -33,6 +38,10 @@ function drawCanvas() {
   }
 }
 
+function updatePaintColor() {
+  state.paintColor = paintColorInput.value;
+}
+
 function updateGridSize(e: Event) {
   const index = gridSizeInput.valueAsNumber;
   const gridSize = gridSizes[index];
@@ -45,7 +54,40 @@ function updateGridSize(e: Event) {
   }
 }
 
+function paint(e: PointerEvent) {
+  if (e.buttons !== 1) return;
+
+  const rect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / rect.width;
+  const scaleY = canvas.height / rect.height;
+
+  const x = (e.clientX - rect.left) * scaleX;
+  const y = (e.clientY - rect.top) * scaleY;
+
+  const cellSize = state.canvasSize / state.gridSize;
+
+  const col = Math.floor(x / cellSize);
+  const row = Math.floor(y / cellSize);
+
+  const drawX = col * cellSize + 1;
+  const drawY = row * cellSize + 1;
+
+  const fillSize = cellSize - 1;
+
+  switch (state.paintingMode) {
+    case "custom-color":
+      ctx.fillStyle = state.paintColor;
+      break;
+  }
+
+  ctx.fillRect(drawX, drawY, fillSize, fillSize);
+}
+
 function setupEvents() {
+  canvas.addEventListener("pointerdown", paint);
+  canvas.addEventListener("pointermove", paint);
+  paintColorInput.addEventListener("input", updatePaintColor);
   gridSizeInput.addEventListener("input", updateGridSize);
   gridSizeInput.addEventListener("change", updateGridSize);
 }
